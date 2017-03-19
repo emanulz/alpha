2348165df6c0fe65ea7b2bd5822ddeba703c3957
Emanuel Zúñiga Infante <emanuzuniga@gmail.com>
2017-03-19 10:39:59 -0600

    0.0.16 Server config added
    Server config added

--- alpha/urls.py
+++ alpha/urls.py
@@ -19,51 +19,11 @@ from django.contrib.auth.decorators import login_required
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
-# from rest_framework import routers
-
-# # Accounting
-# from accounting.accounts.api import AccountViewSet
-# from accounting.entries.api import EntryViewSet, EntryDetailViewSet
-# from accounting.fiscalPeriods.api import FiscalPeriodViewSet
-# from accounting.saleBills.api import SaleBillViewSet, SaleBillDetailViewSet
-
-# Common
-# from common.clients.api import ClientViewSet
-# from common.companies.api import CompanyViewSet
-# from common.recipes.api import RecipeViewSet, RecipeDetailViewSet
-# from common.products.api import ProductViewSet, ProductDepartmentViewSet, ProductSubDepartmentViewSet, ProductForSaleViewSet
-# from common.profiles.api import ProfileViewSet, UserViewSet
-
-#
-# router = routers.DefaultRouter()
-#
-# # # Accounting
-# # router.register(r'accounts', AccountViewSet)
-# # router.register(r'entries', EntryViewSet)
-# # router.register(r'entry_details', EntryDetailViewSet)
-# # router.register(r'fiscal_periods', FiscalPeriodViewSet)
-# # router.register(r'sale_bills', SaleBillViewSet)
-# # router.register(r'sale_bill_details', SaleBillDetailViewSet)
-#
-# # Common
-# router.register(r'clients', ClientViewSet)
-# router.register(r'companies', CompanyViewSet)
-# router.register(r'recipes', RecipeViewSet)
-# router.register(r'recipedetails', RecipeDetailViewSet)
-# router.register(r'products', ProductViewSet)
-# router.register(r'productsforsale', ProductForSaleViewSet)
-# router.register(r'product_departments', ProductDepartmentViewSet)
-# router.register(r'product_subdepartments', ProductSubDepartmentViewSet)
-# router.register(r'profiles', ProfileViewSet)
-# router.register(r'users', UserViewSet)
 
 
 urlpatterns = [
     url(r'^jet/', include('jet.urls', 'jet')),  # Django JET URLS
     url(r'^admin/', include(admin.site.urls)),
-    # url(r'^api/', include(router.urls)),
-    # url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    # url(r'^clients/', include('common.clients.urls')),
     url(r'^sales/', include('sales.urls')),
     url(r'^accounting/', include('accounting.urls')),
     url(r'^general/', include('general.urls')),
